8085735400588201ba30428234ca596e3af221c3
hellopranab15@gmail.com <hellopranab15@gmail.com>
2023-11-19 20:01:40 +0530

    updated and refined extra methods

--- MongoDBConnect/Controllers/PlanetController.cs
+++ MongoDBConnect/Controllers/PlanetController.cs
@@ -20,6 +20,28 @@ namespace MongoDBConnect.Controllers
         public async Task<List<Planet>> Get()
         {
             return await _mongoDBService.GetPlanetsAsync();
-        } 
+        }
+
+        [HttpGet("findByName/{name}")]
+        public async Task<Planet> FindByName(String Name)
+        {
+            var result = await _mongoDBService.GetPlanetByName(Name);
+            if (result == null)
+            {
+                return null;
+            }
+            return result;
+        }
+
+        [HttpPost("Save")]
+        public async Task<Planet> SavePlanet(Planet planet)
+        {
+            var result = await _mongoDBService.SavePlanet(planet);
+            if (result == null)
+            {
+                return null;
+            }
+            return result;
+        }
     }
 }
--- MongoDBConnect/Services/MongoDBService.cs
+++ MongoDBConnect/Services/MongoDBService.cs
@@ -2,6 +2,7 @@
 using MongoDB.Bson;
 using MongoDB.Driver;
 using MongoDBConnect.Models;
+using System.Xml.Linq;
 
 namespace MongoDBConnect.Services
 {
@@ -19,6 +20,32 @@ namespace MongoDBConnect.Services
         public async Task<List<Planet>> GetPlanetsAsync()
         {
             return await _planets.Find(new BsonDocument()).ToListAsync();
+        }
+
+        public async Task<Planet> GetPlanetByName(String Name)
+        {
+            var filter = Builders<Planet>.Filter.Eq("name", Name);
+            var document = await _planets.Find(filter).FirstOrDefaultAsync();
+            var existingPlanet = await _planets.Find(filter).FirstOrDefaultAsync();
+            if (existingPlanet == null)
+            {
+                return null;
+            }
+
+            return document;
+        }
+
+        public async Task<Planet> SavePlanet(Planet planet)
+        {
+            var filter = Builders<Planet>.Filter.Eq("name", planet.Name);
+
+            var existingPlanet = await _planets.Find(filter).FirstOrDefaultAsync();
+            if (existingPlanet == null)
+            {
+                await _planets.InsertOneAsync(planet);
+                return planet;
+            }
+            return null;
         }
     }
 }
